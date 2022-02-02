8ab680fc1e80986870a2c82eee06cfb53ffe0766
RaswanSualdi <you@example.com>
2022-02-02 15:01:15 +0800

    berhasil menyelesaikan sintaks menjumlahkan setiap entrinya

--- forloop.php
+++ forloop.php
@@ -3,18 +3,18 @@
 $x = [1,3,8,9,12];
 $y = [2,4,7,6,11];
  
-// hasilnya nanti [3,7,15,15,23]
-// bagaimana cara menjumlahkan setiap entrinya??
-
-echo $z= count($x);
-
+// // hasilnya nanti [3,7,15,15,23]
+// // bagaimana cara menjumlahkan setiap entrinya??
+// //bagaimana cara menyamakan setiap indeks entrinya??
+ $z= count($x);
+// $value = $x[$i]+1;
 for($i=0;$i<=$z;$i++){
-     $indeksy=$y[$i];
-     $indeksx= $x[$i];
-    if($indeksy==$indeksx){
-        echo $indeksy+$indeksx;
-    } 
+   echo $x[$i]+$y[$i];
 }
 
 
 
+// $angka = array(1, 4, 3, 4);
+// foreach ($x as $value) {
+// echo $value;
+// }
